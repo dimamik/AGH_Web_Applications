3852d6f6c890663deffda37e561253d5a9cd9518
dimamik <dymitrmikielewicz@gmail.com>
2020-11-18 23:21:57 +0100

    Corrected Lab_3 Zad_5_2pkt

--- Labs/Lab_3/Zad_5_2pkt/script.js
+++ Labs/Lab_3/Zad_5_2pkt/script.js
@@ -1,48 +1,84 @@
 /* JavaScript */
-let biggerEl = document.querySelector("#bigger");
-let smallEl = document.querySelector("#small");
-let mediumEl = document.querySelector("#medium");
+let biggerEl = document.getElementById("bigger");
+let smallEl = document.getElementById("small");
+let mediumEl = document.getElementById("medium");
 let totalEl = document.querySelector("#totalCounts");
 let stopStartPropagationEl = document.querySelector("#stopStartPropagation");
+let getAlertsEl = document.querySelector("#getAlerts");
+let resetButtonEl = document.getElementById("resetButton");
 
 let total_value = 0
+let MAX_CLICKS_SMALL = 30;
+let MAX_CLICKES_MEDIUM = 50;
+let MAX_CLICKS_BIG = 55;
 
+biggerEl.addEventListener("click", clickedBigger);
+smallEl.addEventListener("click", clickedSmallEl);
+mediumEl.addEventListener("click", clickedMediumEl);
+resetButtonEl.addEventListener("click", reset);
 
-biggerEl.addEventListener("click",clickedBigger);
-smallEl.addEventListener("click",clickedSmallEl);
-mediumEl.addEventListener("click",clickedMediumEl);
-
-function shouldPropagate(e){
-    if (! stopStartPropagation.checked){
-        e.stopPropagation(); 
+function shouldPropagate(e) {
+    if (!stopStartPropagation.checked) {
+        e.stopPropagation();
     }
 }
 
-function clickedBigger(e){
-    alert("Clicked Blue of Value 1");
-    total_value+=1;
-    shouldPropagate(e);
-    updateTotal();
+function alertIfShould(message) {
+    if (getAlertsEl.checked)
+        alert(message);
 }
-function clickedMediumEl(e){
-    alert("Clicked Red of Value 2");
-    if (total_value<=30)
-        total_value+=2;
-    shouldPropagate(e);
-    updateTotal();
-}
-function clickedSmallEl(e){
 
-    alert("Clicked Yellow of Value 5");
-    if (total_value<=50)
-        total_value+=5;
-    shouldPropagate(e);
-    updateTotal();
-}
-function updateTotal(){
+function updateTotal() {
     totalEl.innerHTML = total_value;
 }
-function reset(){
+
+function reset() {
     total_value = 0;
     updateTotal();
+    mediumEl.style.backgroundColor = "red";
+    smallEl.style.backgroundColor = "yellow";
+    biggerEl.style.backgroundColor = "blue";
+}
+
+function checkIfShouldDisable() {
+    if (total_value >= MAX_CLICKS_SMALL) {
+        mediumEl.style.backgroundColor = "gray";
+    }
+    if (total_value >= MAX_CLICKES_MEDIUM) {
+        smallEl.style.backgroundColor = "gray";
+    }
+    if (total_value > MAX_CLICKS_BIG) {
+        biggerEl.style.backgroundColor = "gray";
+    }
+}
+
+function update(e, message) {
+    shouldPropagate(e);
+    updateTotal();
+    alertIfShould(message);
+    checkIfShouldDisable();
+
+}
+
+function clickedBigger(e) {
+
+    if (total_value <= MAX_CLICKS_BIG)
+        total_value += 1;
+
+    update(e, "Clicked Blue of Value 1");
+}
+
+function clickedMediumEl(e) {
+
+    if (total_value <= MAX_CLICKS_SMALL)
+        total_value += 2;
+
+    update(e, "Clicked Red of Value 2");
+}
+
+function clickedSmallEl(e) {
+    if (total_value <= MAX_CLICKES_MEDIUM)
+        total_value += 5;
+
+    update(e, "Clicked Yellow of Value 5");
 }
